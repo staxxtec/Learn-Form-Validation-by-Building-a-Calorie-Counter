bd10c91ee0b41f3146d86f1f803ee45b43058202
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-05 20:47:48 +0900

    Update script.js
    Step 22
    
    You will need to check if the array ["+", "-", " "] does not include the current character. You can use a combination of the includes() method and the ! operator to do this.
    
    The .includes() method returns true if the array contains the character, and false if not. The logical NOT operator (!) will return the opposite of the value of the .includes() method.
    
    Here is an example checking if the current character is not in the vowels array. If it is not, that current character is pushed into the consonantArray:
    
    const inputString = "Hello World";
    const charArray = inputString.split('');
    const consonantArray = [];
    
    for (let i = 0; i < charArray.length; i++) {
      if (!['a', 'e', 'i', 'o', 'u'].includes(charArray[i])) {
        consonantArray.push(charArray[i]);
      }
    }
    
    Within your loop, you need to check if the character in strArray at index i is not a "+", "-", or a space (" "). If it is not, push it to the cleanStrArray.

--- script.js
+++ script.js
@@ -10,5 +10,7 @@ function cleanInputString (str) {
     const strArray = str.split('');
     const cleanStrArray = [];
     for (let i = 0; i < strArray.length; i++) {
+        if (!["+", "-", " "].includes(strArray[i])) 
+        cleanStrArray.push(strArray[i]);
     }
 }
