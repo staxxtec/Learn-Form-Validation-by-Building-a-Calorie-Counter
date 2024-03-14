fdbb7f13a1649c1e27ea001ad21da812dc5ebaea
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-14 20:21:09 +0900

    Update script.js
    Step 42
    
    Each entry will have a text input for the entry's name, and a number input for the calories. To get a count of the number of entries, you can query by text inputs. Note that you cannot query by number inputs, as you have an extra number input for the user's calorie budget.
    
    Pass the string input[type="text"] to the querySelectorAll() method. Remember that you will need to use single quotes for your string, so that you can use double quotes within.
    
    This will return a NodeList of all the text inputs in the form. You can then access the length property of the NodeList to get the number of entries. Do this on the same line.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function isInvalidInput(str) {
 function addEntry () {
      
      const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-     const entryNumber = targetInputContainer.querySelectorAll();
+     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
 
 
 
