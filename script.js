00e30b81c9601dbe7f3af7d510aa96cbc0424fc6
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-15 17:42:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,9 +26,5 @@ function addEntry () {
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
     <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" ></input>`;
     targetInputContainer.innerHTML += HTMLString;
-    addEntryButton.addEventListener("click", addEntry);
-
-
-
-
 }
+    addEntryButton.addEventListener("click", addEntry);
