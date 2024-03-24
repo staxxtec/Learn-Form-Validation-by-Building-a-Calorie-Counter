86d8da3d8f3718e92b2e20d7209863c5d404f742
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-24 19:40:29 +0900

    Update script.js
    Step 80
    
    Give your span the text remainingCalories Calorie surplusOrDeficit, using interpolation to replace remainingCalories and surplusOrDeficit with the appropriate variables.

--- script.js
+++ script.js
@@ -51,7 +51,8 @@ function calculateCalories(e) {
       const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
       const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
       const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
-      output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}"></span>`;
+      <span class="${surplusOrDeficit.toLowerCase()}">${remainingCalories} Calorie ${surplusOrDeficit}</span>
+  `;
 
 
 
