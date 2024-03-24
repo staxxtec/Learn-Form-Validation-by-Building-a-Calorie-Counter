33f69e1ec6dbf69da2560463aaa1aa3f477113a4
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-24 19:29:16 +0900

    Update script.js
    Step 79
    
    When you need to lower case a string, you can use the toLowerCase() method. This method returns the calling string value converted to lower case.
    
    const firstName = 'JESSICA';
    console.log(firstName.toLowerCase()); // Output: jessica
    
    Your output.innerHTML string will need a span element. Create that, and give it a class attribute set to the surplusOrDeficit variable. Your surplusOrDeficit variable should be converted to lower case using the toLowerCase() method.
    
    Do not give your span any text yet.

--- script.js
+++ script.js
@@ -51,7 +51,8 @@ function calculateCalories(e) {
       const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
       const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
       const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
-      output.innerHTML = ``;
+      output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}"></span>`;
+
 
 
 }
