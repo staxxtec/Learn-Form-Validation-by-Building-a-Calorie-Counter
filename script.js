d2355ae8e3cc325848c74eebc2759fe6fee5a9e0
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-04-04 22:00:32 +0900

    Update script.js
    Step 85
    
    Finally, you need to make the #output element visible so the user can see your text. Your output variable is an Element, which has a classList property. This property has a .remove() method, which accepts a string representing the class to remove from the element.
    
    const paragraphElement = document.getElementById('paragraph');
    paragraphElement.classList.remove('hide');
    
    Use the .remove() method of the output variable's classList property to remove the hide class. Don't forget to place the word hide inside quotes.

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function calculateCalories(e) {
       const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
       const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
       <><><><span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span><hr></hr></><p>${budgetCalories} Calories Budgeted</p></><p>${consumedCalories} Calories Consumed</p><p>${exerciseCalories} Calories Burned</p></>
+      output.classList.remove('hide');
 }
 
 function getCaloriesFromInputs(list) {
