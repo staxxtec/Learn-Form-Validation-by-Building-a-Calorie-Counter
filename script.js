30c5fecc004acc40b6921b744326395106a113e3
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-04-04 22:06:22 +0900

    Update script.js
    Step 87
    
    Your final feature to add is the ability for a user to clear the form. Start by declaring an empty function called clearForm – it should not take any arguments.

--- script.js
+++ script.js
@@ -71,6 +71,10 @@ function getCaloriesFromInputs(list) {
     }
     return calories;
   }
+
+  function clearForm () {
+
+  }
   
   addEntryButton.addEventListener("click", addEntry);
   calorieCounter.addEventListener("submit", calculateCalories);
