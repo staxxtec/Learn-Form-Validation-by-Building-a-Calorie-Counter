8f4d35f85cc5b055ee18c2950dc609c5626af634
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-04-05 08:06:00 +0900

    Update script.js
    Step 89
    
    Remember that document.querySelectorAll returns a NodeList, which is array-like but is not an array. However, the Array object has a .from() method that accepts an array-like and returns an array. This is helpful when you want access to more robust array methods, which you will learn about in a future project.
    
    The following example takes a NodeList of li elements and converts it to an array of li elements:
    
    <ul>
      <li>List 1</li>
      <li>List 2</li>
      <li>List 3</li>
    </ul>
    
    const listItemsArray = Array.from(document.querySelectorAll('li'));
    
    console.log(listItemsArray); //Output: (3) [li, li, li]
    
    Wrap your inputContainers query selector in Array.from(). Do this on the same line as your declaration.

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function getCaloriesFromInputs(list) {
   }
 
   function clearForm () {
-    const inputContainers = document.querySelectorAll('.input-container');
+    const inputContainers = Array.from(document.querySelectorAll('.input-container'));
 
   }
   
