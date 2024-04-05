7e2e8d307863d88b4f7e0522dd9ef84991326216
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-04-05 12:21:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,17 +16,21 @@ function isInvalidInput(str) {
     return str.match(regex);
 }
 
-function addEntry () {
-     
-     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
-     const HTMLString = `
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-    <input type="text" placeholder="Name" id ="${entryDropdown.value}-${entryNumber}-name"></input> 
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" ></input>`;
-    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
-}
+    <input
+      type="number"
+      min="0"
+      id="${entryDropdown.value}-${entryNumber}-calories"
+      placeholder="Calories"
+    />`;
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+  }
 function calculateCalories(e) {
     e.preventDefault();
     isError = false;
